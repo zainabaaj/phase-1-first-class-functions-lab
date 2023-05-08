32e79a2e2147633d1d0dcf5759157a875ccc06a5
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-08 20:07:48 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(driversArray) {
+    return driversArray.slice(0, 2);
+}
+
+function returnLastTwoDrivers(driversArray) {
+    return driversArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driversArray, driversFunction) {
+    return driversFunction(driversArray);
+}
+
